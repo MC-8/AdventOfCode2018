6f8bf42b493fa75ac655c8a5012f892d5ebf1ad1
Max Curzi <max.curzi@cambridgeconsultants.com>
2020-01-03 17:04:59 +0000

    Add day 2 with solutions

---
+++ day_2.py
@@ -0,0 +1,28 @@
+twos = 0
+threes = 0
+for S in open('2.in','r').readlines():
+    for c in S:
+        if S.count(c)==2:
+            twos+=1
+            break
+    for c in S:
+        if S.count(c)==3:
+            threes+=1
+            break
+print(f"Solution part 1 = {twos*threes}")
+done = False
+sol = ''
+for S in open('2.in','r').readlines():
+    for K in open('2.in','r').readlines():
+        count = 0
+        isol = 0
+        for i in range(len(K.strip())):
+            if ord(S[i])-ord(K[i]):
+                count += 1
+                isol = i
+        if count==1:
+            sol = S[:isol]+S[isol+1:]
+            assert (sol == K[:isol]+K[isol+1:])
+    if done:
+        break
+print(f"Solution part 2 = {sol}")
